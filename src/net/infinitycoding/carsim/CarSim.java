013c908e23844617cbb12d3963735f4bcdb1b78b
paul <paul@pbnl.de>
2013-08-19 09:25:59 +0200

    Added CarGenerator.genNewCars()

--- src/net/infinitycoding/carsim/CarSim.java
+++ src/net/infinitycoding/carsim/CarSim.java
@@ -2,13 +2,15 @@ package net.infinitycoding.carsim;
 
 import java.util.ArrayList;
 
+import net.infinitycoding.carsim.modules.Car;
 import net.infinitycoding.carsim.testing.Test;
 import net.infinitycoding.carsim.util.CarGenerator;
 
 public class CarSim {
 	
 	private boolean run = false;
-	private ArrayList Cars = new ArrayList();
+	private ArrayList<Car> cars = new ArrayList<Car>();
+	private CarGenerator generator;
 
 	public static void main(String[] args) {
 		
@@ -23,13 +25,16 @@ public class CarSim {
 		
 		UserInterface userInterface = new UserInterface();
 		
-		CarGenerator generator = new CarGenerator();
+		generator = new CarGenerator();
 		
 		while(this.run){
 			//Hauptschleife
 			difTime = afterTime - beforeTime;
 			beforeTime = System.currentTimeMillis();
+			
+			this.Cars.addAll(this.generator.genNewCars());
 			this.moveCars(difTime);
+			
 			afterTime = System.currentTimeMillis();
 		}
 	}
@@ -40,12 +45,12 @@ public class CarSim {
 		
 	}
 
-	public boolean isRun()
+	public boolean isRunning()
 	{
 		return run;
 	}
 
-	public void setRun(boolean run)
+	public void setRunning(boolean run)
 	{
 		this.run = run;
 	}
--- src/net/infinitycoding/carsim/util/CarGenerator.java
+++ src/net/infinitycoding/carsim/util/CarGenerator.java
@@ -1,6 +1,14 @@
 package net.infinitycoding.carsim.util;
 
+import java.util.Collection;
+
 public class CarGenerator
 {
+
+	public Collection genNewCars()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
