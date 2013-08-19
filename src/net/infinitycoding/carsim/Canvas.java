1d909f26f80cd97a9c4561f7b4c11b7df9ecf96f
MrPixelDream <carpetsoftware@yahoo.com>
2013-08-19 09:14:16 +0200

    Created Canvas class and modified UserInterface for using the new Canvas

---
+++ src/net/infinitycoding/carsim/Canvas.java
@@ -0,0 +1,12 @@
+package net.infinitycoding.carsim;
+
+import javax.swing.JPanel;
+
+public class Canvas extends JPanel {
+	
+	public Canvas(int width, int height)
+	{
+		this.setBounds(0, 0, width, height);
+	}
+
+}
