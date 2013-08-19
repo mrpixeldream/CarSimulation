812c0019a64355cbee48f4224a71b39ae8372fb4
paul <paul@pbnl.de>
2013-08-19 08:58:52 +0200

    Added a mainloop and getter/setters to control them

--- src/net/infinitycoding/carsim/CarSim.java
+++ src/net/infinitycoding/carsim/CarSim.java
@@ -4,10 +4,28 @@ import net.infinitycoding.carsim.testing.Test;
 
 public class CarSim {
 	
+	private boolean run;
+
 	public static void main(String[] args) {
 		
 		new Test().start();
 		
 	}
 	
+	public void start(){
+		while(this.run){
+			//Hauptschleife
+			
+		}
+	}
+	
+	public boolean isRun()
+	{
+		return run;
+	}
+
+	public void setRun(boolean run)
+	{
+		this.run = run;
+	}
 }
