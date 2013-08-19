f13369ba0065b943fa6e2ae23f3255a8ef1d07bd
paul <paul@pbnl.de>
2013-08-19 09:18:40 +0200

    Generate a UserInterface and the CarGenerator in CarSim.start(), Added the CarSim.moveCars() and a thing to calculate the time between to Frames

--- src/net/infinitycoding/carsim/CarSim.java
+++ src/net/infinitycoding/carsim/CarSim.java
@@ -1,10 +1,14 @@
 package net.infinitycoding.carsim;
 
+import java.util.ArrayList;
+
 import net.infinitycoding.carsim.testing.Test;
+import net.infinitycoding.carsim.util.CarGenerator;
 
 public class CarSim {
 	
-	private boolean run;
+	private boolean run = false;
+	private ArrayList Cars = new ArrayList();
 
 	public static void main(String[] args) {
 		
@@ -13,12 +17,29 @@ public class CarSim {
 	}
 	
 	public void start(){
+		long beforeTime = 0;
+		long afterTime = 0;
+		long difTime = 0;
+		
+		UserInterface userInterface = new UserInterface();
+		
+		CarGenerator generator = new CarGenerator();
+		
 		while(this.run){
 			//Hauptschleife
-			
+			difTime = afterTime - beforeTime;
+			beforeTime = System.currentTimeMillis();
+			this.moveCars(difTime);
+			afterTime = System.currentTimeMillis();
 		}
 	}
 	
+	private void moveCars(long difTime)
+	{
+		
+		
+	}
+
 	public boolean isRun()
 	{
 		return run;
