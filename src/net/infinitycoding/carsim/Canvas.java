fbd9641723d107d5dcc4043f1b901c3eb1f6b83a
paul <paul@pbnl.de>
2013-08-23 13:12:02 +0200

    Corrected the .jpg to .png

--- src/net/infinitycoding/carsim/Canvas.java
+++ src/net/infinitycoding/carsim/Canvas.java
@@ -10,7 +10,7 @@ public class Canvas extends JPanel
 	{
 		this.setBounds(0, 0, width, height);
 		
-		JLabel bgImg = new JLabel(new ImageIcon(CarSim.class.getResource("res/straße.jpg")));
+		JLabel bgImg = new JLabel(new ImageIcon(CarSim.class.getResource("res/straße.png")));
 		bgImg.setBounds(0, 0, width, height);
 		bgImg.setBorder(null);
 		
