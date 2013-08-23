28b95a924d84340fe558a85b3dffac346a561cb3
MrPixelDream <carpetsoftware@yahoo.com>
2013-08-23 12:52:29 +0200

    Set a window title and tried to fix the border issue again

--- src/net/infinitycoding/carsim/UserInterface.java
+++ src/net/infinitycoding/carsim/UserInterface.java
@@ -1,18 +1,23 @@
 package net.infinitycoding.carsim;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
-public class UserInterface extends JFrame {
-	
+public class UserInterface extends JFrame
+{	
 	Canvas canvas;
 	
 	public UserInterface()
 	{
 		super();
 		
-		this.setBounds(0, 0, 1280, 1000);
+		this.setBounds(0, 0, 1280, 1024);
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setTitle("CarSimulator by InfinityCoding");
+		this.setLayout(new GridLayout(1, 1));
 		
 		this.canvas = new Canvas(1280, 1024);
 		this.add(canvas);
