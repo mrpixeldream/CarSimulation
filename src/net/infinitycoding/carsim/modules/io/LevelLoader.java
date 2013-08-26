3f8e0f4843ab112f9f5c962dbb3e3c5de6a61dd0
MrPixelDream <carpetsoftware@yahoo.com>
2013-08-26 11:37:29 +0200

    Edited LevelLoader to read the file, splitting the config is coming soon

--- src/net/infinitycoding/carsim/modules/io/LevelLoader.java
+++ src/net/infinitycoding/carsim/modules/io/LevelLoader.java
@@ -1,6 +1,38 @@
 package net.infinitycoding.carsim.modules.io;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
+import net.infinitycoding.carsim.modules.Level;
+
 public class LevelLoader
 {
-
+	public static Level loadLevel(String lvlFileName)
+	{
+		File lvlFile = new File(lvlFileName);
+		try
+		{
+			Scanner scr = new Scanner(lvlFile);
+			StringTokenizer tokenizer;
+			
+			while (scr.hasNext())
+			{
+				String line = scr.nextLine();
+				tokenizer = new StringTokenizer(line, "=");
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Datei nicht gefunden!", "Dateifehler", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		return null;
+	}
 }
