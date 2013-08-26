ef66ff4b22ba508704fd64f4b25db2bd68ab4234
paul (nkpmedia) <paul@pbnl.de>
2013-08-26 10:47:22 +0200

    Added a status of to trafficlight

--- src/net/infinitycoding/carsim/modules/TrafficLight.java
+++ src/net/infinitycoding/carsim/modules/TrafficLight.java
@@ -2,5 +2,14 @@ package net.infinitycoding.carsim.modules;
 
 public class TrafficLight
 {
-
+	private boolean isOn;
+	
+	public void setOn(boolean on)
+	{
+		this.isOn= on;
+	}
+	public boolean getOn()
+	{
+		return this.isOn;
+	}
 }
