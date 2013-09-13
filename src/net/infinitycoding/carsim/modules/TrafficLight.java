04f9900105d8a923eef4c9994e85470a69a64f63
nkpmedia <paul@pbnl.de>
2013-09-13 12:52:04 +0200

    Added a constructor to TrafficLight

--- src/net/infinitycoding/carsim/modules/TrafficLight.java
+++ src/net/infinitycoding/carsim/modules/TrafficLight.java
@@ -6,6 +6,12 @@ public class TrafficLight
 	public int x;
 	public int y;
 	
+	public TrafficLight(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
 	public void setOn(boolean on)
 	{
 		this.isOn= on;
