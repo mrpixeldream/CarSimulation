bba5c290a02f1ce5d7b0c0f7cb0321e0a3fd195e
nkpmedia <paul@pbnl.de>
2013-10-25 13:30:30 +0200

    TMP

--- src/net/infinitycoding/carsim/UserInterface.java
+++ src/net/infinitycoding/carsim/UserInterface.java
@@ -1,9 +1,11 @@
 package net.infinitycoding.carsim;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,10 +20,15 @@ import net.infinitycoding.carsim.modules.Street;
 public class UserInterface extends JFrame
 {	
 	Canvas canvas;
+	private ImageIcon background;
+	public BufferStrategy b;
+	private Graphics graphic;
 	
 	public UserInterface(ImageIcon background, CarSim carSim)
 	{
 		super();
+		
+		this.background = background;
 		
 		this.setBounds(0, 0, 1280, 1024);
 		
@@ -33,9 +40,14 @@ public class UserInterface extends JFrame
 		
 		this.canvas = new Canvas(1280, 1024, background,carSim);
 		this.setContentPane(canvas);
-		
-		
 		this.setVisible(true);
+		
+		setIgnoreRepaint(true);
+		this.createBufferStrategy(2);
+		this.b = getBufferStrategy();
+		this.graphic = this.b.getDrawGraphics();
+		
+		
 	}
 
 	public void checkCollision()
@@ -47,7 +59,7 @@ public class UserInterface extends JFrame
 	{
 		for(Car elem : cars)
 		{
-			canvas.getGraphics().drawImage(elem.picture, elem.x, elem.y, null);
+			this.graphic.drawImage(elem.picture, elem.x, elem.y, null);
 		}
 	}
 
@@ -56,11 +68,11 @@ public class UserInterface extends JFrame
 		{
 			if(streets.get(street).trafficLight.getOn())
 			{
-				this.canvas.getGraphics().drawImage(streets.get(street).trafficLight.greenLight,streets.get(street).trafficLight.x,streets.get(street).trafficLight.y,null);
+				this.graphic.drawImage(streets.get(street).trafficLight.greenLight,streets.get(street).trafficLight.x,streets.get(street).trafficLight.y,null);
 			}
 			else
 			{
-				this.canvas.getGraphics().drawImage(streets.get(street).trafficLight.redLight,streets.get(street).trafficLight.x,streets.get(street).trafficLight.y,null);
+				this.graphic.drawImage(streets.get(street).trafficLight.redLight,streets.get(street).trafficLight.x,streets.get(street).trafficLight.y,null);
 			}
 			
 		}
